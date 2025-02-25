aa8c9ad7f3e4f1218218cc1dc59702b732c120f3
Sjuniorr6 <sjuniorr6@gmail.com>
2025-02-25 13:01:47 -0300

    mapa33

--- acionamento/settings.py
+++ acionamento/settings.py
@@ -25,10 +25,16 @@ INSTALLED_APPS = [
     'core',  # Seu aplicativo principal
     'formacompanhamento',
     'widget_tweaks',
+    'corsheaders',
     
 
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://gsacionamento.com",
+    "https://www.gsacionamento.com",
+    "http://127.0.0.1:8000",
+]
 # Middlewares
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -39,6 +45,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'formacompanhamento.middleware.LoginRequiredMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 # Configuração de URL raiz
