95d6022dfcf9ac4a98f39034a2b94a0e8be6b0f4
Sjuniorr6 <sjuniorr6@gmail.com>
2025-04-28 15:02:49 -0300

    asgi

--- acionamento/asgi.py
+++ acionamento/asgi.py
@@ -13,13 +13,16 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acionamento.settings')
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from realtime_notifications.routing import websocket_urlpatterns
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
-    ),
-})
+def get_application():
+    from realtime_notifications.routing import websocket_urlpatterns
+    return ProtocolTypeRouter({
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                websocket_urlpatterns
+            )
+        ),
+    })
+
+application = get_application()
