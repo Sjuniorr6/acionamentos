a634590595723328a35839389d9ffc75f0d8c593
Sjuniorr6 <sjuniorr6@gmail.com>
2025-05-07 15:46:40 -0300

    api/

--- acionamento/urls.py
+++ acionamento/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from formacompanhamento.views import api_todos_acionamentos
+from formacompanhamento.views import api_todos_acionamentos,detalhar_acionamento_endpoint
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +10,8 @@ urlpatterns = [
     path('', include('core.urls')),
     path('api/todos_acionamentos/', api_todos_acionamentos, name='api_todos_acionamentos'),
     path('formacompanhamento/', include('formacompanhamento.urls')),
+    path('api/registro_pagamento/<int:pk>/detalhes/', detalhar_acionamento_endpoint, name='detalhar_acionamento_endpoint'),
+
 ]
 
 # Configuração para servir arquivos de mídia no modo DEBUG
