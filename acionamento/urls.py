e8101fe8c1a21f90f4b899af2e1662e0a8b1e8a0
Sjuniorr6 <sjuniorr6@gmail.com>
2025-05-07 13:56:27 -0300

    endpointAtualizado

--- acionamento/urls.py
+++ acionamento/urls.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from formacompanhamento.urls import all_prestadores_addresses,detalhar_acionamento_endpoint
+from formacompanhamento.views import api_todos_acionamentos
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('notifications/', include('realtime_notifications.urls')),
     path('', include('core.urls')),
-    path('api/prestadores/enderecos/', all_prestadores_addresses, name='all_prestadores_addresses'),
+    path('api/todos_acionamentos/', api_todos_acionamentos, name='api_todos_acionamentos'),
     path('formacompanhamento/', include('formacompanhamento.urls')),
-    path('api/registro_pagamento/<int:pk>/detalhes/', detalhar_acionamento_endpoint, name='detalhar_acionamento_endpoint'),
 ]
 
 # Configuração para servir arquivos de mídia no modo DEBUG
