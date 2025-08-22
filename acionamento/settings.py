8007b9ecb5b5ad92b147e9b8e4a420af0c51ec55
Sjuniorr6 <sjuniorr6@gmail.com>
2025-08-22 12:00:34 -0300

    atualizacoes esporadico

--- acionamento/settings.py
+++ acionamento/settings.py
@@ -86,29 +86,24 @@ WSGI_APPLICATION = 'acionamento.wsgi.application'
 
 
 # Configuração do banco de dados (SQLite para desenvolvimento)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 # Configuração PostgreSQL (comentada para desenvolvimento local)
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',  # ou o nome do banco que você definiu
-#         'USER': 'postgres',  # ou o usuário mestre que você criou
-#         'PASSWORD': '44523913',
-#         'HOST': 'database-1.cfegu84mu8gn.us-east-1.rds.amazonaws.com',
-#         'PORT': '5432',
-#         'OPTIONS': {
-#             'sslmode': 'verify-full',
-#             'sslrootcert': '/var/www/acionamentos/.postgresql/root.crt',
-#             'options': '-c timezone=America/Sao_Paulo'
-#         }
-#     }
-# }
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'postgres',  # ou o nome do banco que você definiu
+         'USER': 'postgres',  # ou o usuário mestre que você criou
+         'PASSWORD': '44523913',
+         'HOST': 'database-1.cfegu84mu8gn.us-east-1.rds.amazonaws.com',
+         'PORT': '5432',
+         'OPTIONS': {
+             'sslmode': 'verify-full',
+             'sslrootcert': '/var/www/acionamentos/.postgresql/root.crt',
+             'options': '-c timezone=America/Sao_Paulo'
+         }
+     }
+ }
 
 
 
