b64dc1090eccf74c84d1836773b167a89957b10f
Sjuniorr6 <sjuniorr6@gmail.com>
2025-08-29 10:30:34 -0300

    update

--- acionamento/settings.py
+++ acionamento/settings.py
@@ -137,14 +137,7 @@ if os.environ.get('DJANGO_ENV') == 'production':
             }
         }
     }
-else:
-    # Configuração SQLite para desenvolvimento local
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+
 
 
 
